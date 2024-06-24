ab8656379e441eee5cab4c7d763a41178e2ae18d
Asik8 <motasembillah84@gmail.com>
2024-06-24 06:41:11 +0600

    2 pointer derefferencing done

--- 2_Pointer_Dereference.cpp
+++ 2_Pointer_Dereference.cpp
@@ -4,10 +4,10 @@ using namespace std;
 #define pb push_back
 #define pob pop_back
 
-void f(int *ptr)
+void f(int *&ptr)    // here p and ptr pointing on a same location. their address is same. Because we are dereferencing pointer p.
 {
     *ptr = 20;      // changing value of val
-    ptr = NULL;     // making ptr NULL, which does not affect p pf the main function
+    ptr = NULL;     // making ptr NULL, which does not affect p pf the main function. after commenting this line code will work normally. Else nothing will be there as output.
 }
 
 int main() {
@@ -15,6 +15,6 @@ int main() {
     int *p = &val;
     f(p);           // passing the pointer in the f function.
     cout<<*p<<endl; // printing val using pointer.
-
+    cout<<val<<endl; // p has the address of val. we changed the address into NULL. So, nothing will be printed.
     return 0;
 }
